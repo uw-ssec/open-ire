7a8b9aceffd51dcca5efed4afe28a5937c57e0e2
Richard Boyechko <boyechko@uw.edu>
2026-02-05 11:57:12 -0800

    feat(logging): Add OpenIRELogger extension and OPEN_IRE_LOG_LEVEL
    This allows a finer control over logging during the development.

--- src/open_ire/settings/development.py
+++ src/open_ire/settings/development.py
@@ -1,7 +1,10 @@
 from .base import *  # noqa: F403
 from .base import ITEM_PIPELINES
 
-LOG_LEVEL = "DEBUG"
+EXTENSIONS = {"open_ire.logging.OpenIRELogger": 100}
+LOG_LEVEL = "WARNING"
+OPEN_IRE_LOG_LEVEL = "DEBUG"
+
 SHAREPOINT_BASE_PATH = "open_ire_dev"
 
 if "open_ire.pipelines.SharePointPipeline" in ITEM_PIPELINES:
